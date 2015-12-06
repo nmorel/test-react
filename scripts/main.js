30e84ca4909588d290b2b2554f09dbfe5f01705c
Nicolas Morel <nmr.morel@gmail.com>
2015-12-06 12:50:17 +0100

    Fix the left menu depending of the scroll position

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,44 @@
-(function () {
+(function() {
     "use strict";
 
-    var headerNav = document.getElementById("header-nav");
-    document.getElementById("header-nav-toggle").addEventListener("click", toggleHeaderNav);
+    handleToggleNav();
+    handleLeftMenu();
 
     /**
-     * Toggle the navigation menu in the header
+     * Handle the toggle button to show/hide the menu
      */
-    function toggleHeaderNav() {
-        if (headerNav.className.indexOf('opened') > 0) {
-            headerNav.className = headerNav.className.replace('opened', '');
-        } else {
-            headerNav.className = headerNav.className + ' opened';
+    function handleToggleNav() {
+        var headerNav = document.getElementById("header-nav");
+        document.getElementById("header-nav-toggle").addEventListener("click", toggleHeaderNav);
+
+        /**
+         * Toggle the navigation menu in the header
+         */
+        function toggleHeaderNav() {
+            if (headerNav.className.indexOf('opened') > 0) {
+                headerNav.className = headerNav.className.replace('opened', '');
+            } else {
+                headerNav.className = headerNav.className + ' opened';
+            }
+        }
+    }
+
+
+    /**
+     * Handle the position of the left menu
+     */
+    function handleLeftMenu() {
+        var header = document.getElementById("header");
+        var leftMenu = document.getElementById("left-nav");
+        updateLeftMenuTop();
+        document.addEventListener("scroll", updateLeftMenuTop);
+
+        /**
+         * Update the position of the left menu
+         */
+        function updateLeftMenuTop() {
+            var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+            leftMenu.style.top = Math.max(0, header.offsetHeight - scrollTop) + 'px';
         }
     }
 })();
